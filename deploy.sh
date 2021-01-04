0bee379557ca9f40217c72cca859a3d11529a1ea
F. Javier del Castillo <javicastillo92@gmail.com>
2021-01-05 00:30:24 +0100

    deploy script working

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+GIT_REPO_URL=$(git config --get remote.origin.url)
+GIT_CURRENT_BRANCH=$(git branch --show-current)
+
+mkdir .deploy
+cp -R ./docs .deploy
+cd .deploy
+git init .
+git remote add github $GIT_REPO_URL
+git checkout -b gh-pages
+git add .
+git commit -am "Static site deploy"
+git push github gh-pages --force
+cd ..
+rm -rf .deploy
+git checkout $GIT_CURRENT_BRANCH
+echo "Finished deploy"
+
+exit 0
