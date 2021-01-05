d95c19005cb8ed724b11a3b06f177b221076b0a1
F. Javier del Castillo <javicastillo92@gmail.com>
2021-01-05 01:18:55 +0100

    script WIP

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 GIT_REPO_URL=$(git config --get remote.origin.url)
 GIT_CURRENT_BRANCH=$(git branch --show-current)
+DATE=`date +'%a, %Y-%m-%d %T'`
 
 npm run build
 
-mkdir .deploy
-cp -R ./docs .deploy
-cd .deploy
-git init .
-git remote add github $GIT_REPO_URL
 git checkout --orphan gh-pages
-git add .
-git commit -am "Static site deploy"
-git push github gh-pages --force
-cd ..
-rm -rf .deploy
+git add docs
+git commit -am "deploy $DATE"
+git push origin gh-pages --force
 git checkout $GIT_CURRENT_BRANCH
+
 echo " "
 echo "[ + Finished deploy! ]"
 echo " "
