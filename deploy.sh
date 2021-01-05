359cf11d307991b06deea8471c690648627e3a13
F. Javier del Castillo <javicastillo92@gmail.com>
2021-01-05 01:08:50 +0100

    new build

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,22 @@
 GIT_REPO_URL=$(git config --get remote.origin.url)
 GIT_CURRENT_BRANCH=$(git branch --show-current)
 
+npm run build
+
 mkdir .deploy
 cp -R ./docs .deploy
 cd .deploy
 git init .
 git remote add github $GIT_REPO_URL
-git checkout -b gh-pages
+git checkout --orphan gh-pages
 git add .
 git commit -am "Static site deploy"
 git push github gh-pages --force
 cd ..
 rm -rf .deploy
 git checkout $GIT_CURRENT_BRANCH
-echo "Finished deploy"
+echo " "
+echo "[ + Finished deploy! ]"
+echo " "
 
 exit 0
