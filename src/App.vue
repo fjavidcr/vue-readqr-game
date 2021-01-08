578060c62beea089564c191868ea373f07e6e589
F. Javier del Castillo <javicastillo92@gmail.com>
2021-01-08 16:41:40 +0100

    qr generator working

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <QrGenerator :url="url"/>
     <pre>Secret Key: {{key}}</pre>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import QrGenerator from './components/QrGenerator.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, 
+    QrGenerator
   },
   data() { 
     return { 
-      key: process.env.VUE_APP_KEY
+      key: process.env.VUE_APP_KEY,
+      url: "https://www.npmjs.com/package/qrcode"
     }
   }
 }
---
+++ src/components/QrGenerator.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+    <img id="qrImage" alt="" srcset="">
+  </div>
+</template>
+
+<script>
+import QRCode from 'qrcode'
+ 
+// // With promises
+// QRCode.toDataURL('I am a pony!')
+//   .then(url => {
+//     console.log(url)
+//   })
+//   .catch(err => {
+//     console.error(err)
+//   })
+ 
+// With async/await
+
+export default {
+  name: 'QrGenerator',
+  props: {
+    url: String
+  },
+  watch: {
+    url (newURL) {
+      this.generateQR(newURL, 'qrImage')
+    }
+  },
+  mounted: function () {
+    this.generateQR(this.url, 'qrImage')
+  },
+  methods: {
+    generateQR: async (text, element) => {
+      var opts = {
+        errorCorrectionLevel: 'H',
+        type: 'image/png',
+        quality: 0.5,
+        margin: 3,
+        color: {
+          dark:"#010599FF",
+          light:"#FFBF60FF"
+        }
+      }
+      try {
+        let uri = await QRCode.toDataURL(text, opts)
+        console.log('qrURI')
+        console.log(uri)
+        if (element) document.getElementById(element).src = uri
+        return uri
+      } catch (err) {
+        console.error(err)
+      }
+    }
+  }
+}
+</script>
